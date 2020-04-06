222c51e963519811858d1c155145401a93c4a147
scorebrain <42388998+scorebrain@users.noreply.github.com>
2020-04-06 17:22:11 -0400

    Head First Android Development Sample Demonstrates Fragments

---
+++ HF_Workout/app/src/main/java/com/electromech/hf_workout/Workout.java
@@ -0,0 +1,35 @@
+package com.electromech.hf_workout;
+
+public class Workout {
+    private String name;
+    private String description;
+
+    public static final Workout[] workouts = {
+            new Workout("The Limb Loosener",
+                    "5 Handstand push-ups\n10 1-legged squats\n15 Pull-ups"),
+            new Workout("Core Agony",
+                    "100 Pull-ups\n100 Push-ups\n100 Sit-up\n100 Squats"),
+            new Workout("The Wimp Special",
+                    "5 Pull-ups\n10 Push-ups\n15 Squats"),
+            new Workout("Strength and Length",
+                    "500-meter Run\n21 x 1.5-pound Kettleball Swing\n21 x Pull-ups")
+    };
+
+    //Each Working has a name and description
+    private Workout(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String toString() {
+        return this.name;
+    }
+}
