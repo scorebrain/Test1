ce2ae11e37ca0637864cf03e1e95ab0f7355ae83
scorebrain <42388998+scorebrain@users.noreply.github.com>
2020-04-08 00:05:14 -0400

    Head First Android Development CatChat Mail App

---
+++ HF_CatChat/app/src/main/java/com/electromech/hf_catchat/MainActivity.java
@@ -0,0 +1,23 @@
+package com.electromech.hf_catchat;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Fragment fragment = new InboxFragment();
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.add(R.id.content_frame, fragment);
+        ft.commit();
+    }
+}
