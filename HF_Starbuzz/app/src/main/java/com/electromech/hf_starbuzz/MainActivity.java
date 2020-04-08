53e52214074ae2227a074757460d6039c84eb59c
scorebrain <42388998+scorebrain@users.noreply.github.com>
2020-04-08 07:52:06 -0400

    Head First Android Development Example ...with Java classes instead of databaase

---
+++ HF_Starbuzz/app/src/main/java/com/electromech/hf_starbuzz/MainActivity.java
@@ -0,0 +1,36 @@
+package com.electromech.hf_starbuzz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Create an OnItemClickListener
+        AdapterView.OnItemClickListener itemClickListener =
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> listView,
+                                            View itemView,
+                                            int position,
+                                            long id) {
+                        if (position == 0) {
+                            Intent intent = new Intent(MainActivity.this,
+                                    DrinkCategoryActivity.class);
+                            startActivity(intent);
+                        }
+                    }
+                };
+        //Add the listener to the list view
+        ListView listView = (ListView) findViewById(R.id.list_options);
+        listView.setOnItemClickListener(itemClickListener);
+    }
+}
