d634692f38d39a6359fff872b11aedc057abcd74
scorebrain <42388998+scorebrain@users.noreply.github.com>
2020-04-13 13:40:38 -0400

    Head First Android Development Example ...with database instead of Java Classes

--- HF_Starbuzz/app/src/main/java/com/electromech/hf_starbuzz/DrinkActivity.java
+++ HF_Starbuzz/app/src/main/java/com/electromech/hf_starbuzz/DrinkActivity.java
@@ -2,9 +2,15 @@ package com.electromech.hf_starbuzz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
 public class DrinkActivity extends AppCompatActivity {
 
@@ -17,7 +23,48 @@ public class DrinkActivity extends AppCompatActivity {
 
         //Get the drink from the intent
         int drinkId = (Integer) getIntent().getExtras().get(EXTRA_DRINKID);
-        Drink drink = Drink.drinks[drinkId];
+        //Drink drink = Drink.drinks[drinkId];
+
+        //Create a cursor
+        SQLiteOpenHelper starbuzzDatabaseHelper = new StarbuzzDatabaseHelper(this);
+        try {
+            SQLiteDatabase db = starbuzzDatabaseHelper.getReadableDatabase();
+            Cursor cursor = db.query("DRINK",
+                    new String[] {"NAME", "DESCRIPTION", "IMAGE_RESOURCE_ID"},
+                    "_id = ?",
+                    new String[] {Integer.toString(drinkId)},
+                    null, null, null);
+            //Move to the first record in the Cursor
+            if (cursor.moveToFirst()) {
+
+                //Get the drink details from the cursor
+                String nameText = cursor.getString(0);
+                String descriptionText = cursor.getString(1);
+                int photoId = cursor.getInt(2);
+
+                //Populate the drink name
+                TextView name = (TextView) findViewById(R.id.name);
+                name.setText(nameText);
+
+                //Populate the drink description
+                TextView description = (TextView) findViewById(R.id.description);
+                description.setText(descriptionText);
+
+                //Populate the drink image
+                ImageView photo = (ImageView) findViewById(R.id.photo);
+                photo.setImageResource(photoId);
+                photo.setContentDescription(nameText);
+            }
+            cursor.close();
+            db.close();
+        } catch (SQLiteException e) {
+            Toast toast = Toast.makeText(this,
+                    "Database unavailable",
+                    Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
+        /*  Old code for pulling data from Java class
 
         //Populate the drink name
         TextView name = (TextView) findViewById(R.id.name);
@@ -31,5 +78,7 @@ public class DrinkActivity extends AppCompatActivity {
         ImageView photo = (ImageView) findViewById(R.id.photo);
         photo.setImageResource(drink.getImageResourceId());
         photo.setContentDescription(drink.getName());
+
+         */
     }
 }
--- HF_Starbuzz/app/src/main/java/com/electromech/hf_starbuzz/DrinkCategoryActivity.java
+++ HF_Starbuzz/app/src/main/java/com/electromech/hf_starbuzz/DrinkCategoryActivity.java
@@ -3,25 +3,52 @@ package com.electromech.hf_starbuzz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 public class DrinkCategoryActivity extends AppCompatActivity {
+
+    private SQLiteDatabase db;
+    private Cursor cursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drink_category);
-        //setContentView(R.layout.activity_main);
+        /* Old code for using data from Java class
         ArrayAdapter<Drink> listAdapter = new ArrayAdapter<>(
                 this,
                 android.R.layout.simple_list_item_1,
                 Drink.drinks);
         ListView listDrinks = (ListView) findViewById(R.id.list_drinks);
         listDrinks.setAdapter(listAdapter);
+         */
+        ListView listDrinks = (ListView) findViewById(R.id.list_drinks);
+        SQLiteOpenHelper starbuzzDatabaseHelper = new StarbuzzDatabaseHelper(this);
+        try {
+            db = starbuzzDatabaseHelper.getReadableDatabase();
+            cursor = db.query("DRINK",
+                    new String[] {"_id", "NAME"},
+                    null, null, null, null, null);
+            SimpleCursorAdapter listAdapter = new SimpleCursorAdapter(this,
+                    android.R.layout.simple_list_item_1,
+                    cursor,
+                    new String[] {"NAME"},
+                    new int[] {android.R.id.text1},
+                    0);
+            listDrinks.setAdapter(listAdapter);
+        } catch (SQLiteException e) {
+            Toast toast = Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT);
+            toast.show();
+        }
 
         //Create the listener
         AdapterView.OnItemClickListener itemClickListener =
@@ -39,5 +66,12 @@ public class DrinkCategoryActivity extends AppCompatActivity {
                 };
         //Assign the listener to the list view
         listDrinks.setOnItemClickListener(itemClickListener);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        cursor.close();
+        db.close();
     }
 }
